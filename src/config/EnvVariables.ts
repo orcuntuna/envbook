fb1ff48898449e6b2ad725809794ea57a00cd104
Orçun Tuna <turboindirblog@gmail.com>
2023-02-17 20:48:07 +0300

    add env variables and system config files

---
+++ src/config/EnvVariables.ts
@@ -0,0 +1,6 @@
+export const EnvVariables = {
+  public: {
+    companyName: process.env.NEXT_PUBLIC_COMPANY_NAME,
+  },
+  private: {},
+}
---
+++ src/config/System.ts
@@ -0,0 +1,4 @@
+export const System = {
+  projectName: 'EnvBook',
+  projectGithubUrl: 'https://github.com/orcuntuna/envbook',
+}
